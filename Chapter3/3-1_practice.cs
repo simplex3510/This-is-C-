e632974092821103276d1f150ad65c34b59cb201
simplex3510 <leeyj6457@naver.com>
2021-07-17 17:33:37 +0900

    3-1번 풀이

--- Chapter3/3-1_practice.cs
+++ Chapter3/3-1_practice.cs
@@ -1,12 +1,19 @@
 ﻿using System;
 
-namespace c__education
+namespace RectArea
 {
-    class Program
+    class MainApp
     {
         static void Main(string[] args)
         {
-           
+            Console.WriteLine("사각형의 너비를 입력하세요.");
+            string width = Console.ReadLine();
+
+            Console.WriteLine("사각형의 높이를 입력하세요.");
+            string height = Console.ReadLine();
+
+            double area = double.Parse(width) * double.Parse(height);
+            Console.WriteLine($"{"사각형의 넓이는 : "}{area}");
         }
     }
 }
